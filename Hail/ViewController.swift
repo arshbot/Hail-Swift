135eee0623beb236931fd9160054ae024a79ee11
Harsha Goli <harshagoli@Harshas-MacBook-Pro.local>
2017-07-31 20:19:53 -0400

    Initial Commit

---
+++ Hail/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Hail
+//
+//  Created by Harsha Goli on 7/31/17.
+//  Copyright © 2017 Harsha Goli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
