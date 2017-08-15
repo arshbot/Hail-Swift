a227f8cd3cb00fe22d90c5386e0320e0e0230276
Harsha Goli <harshagoli@gmail.com>
2017-08-15 14:46:12 -0400

    Inital Commit :tada:

---
+++ Hail/NewTransactionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  NewTransactionViewController.swift
+//  Hail
+//
+//  Created by Harsha Goli on 8/15/17.
+//  Copyright © 2017 Harsha Goli. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NewTransactionViewController: UIViewController {
+    
+}
