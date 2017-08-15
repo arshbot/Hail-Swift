68f4e132ecb99625118a9873e5dc8642d57af498
Harsha Goli <harshagoli@gmail.com>
2017-08-15 19:14:36 -0400

    Added delegate functionality for NewTxVC

--- Hail/NewTransactionViewController.swift
+++ Hail/NewTransactionViewController.swift
@@ -9,6 +9,57 @@
 import Foundation
 import UIKit
 
-class NewTransactionViewController: UIViewController {
+class NewTransactionViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var destination: UITextField!
     
+    @IBAction func sendTx(_ sender: Any) {
+        if selectedWallet.name == "null" {
+            let alert = UIAlertController(title: "Alert", message: "Please select a wallet", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        } else {
+            
+        }
+    }
+    
+    @IBOutlet weak var walletSelectionTableView: UITableView!
+    
+    let dataManager = DataManager(coin: "All")
+    
+    var wallets:[CryptoWallet] = []
+    
+    var selectedWallet = CryptoWallet()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        walletSelectionTableView.delegate = self
+        walletSelectionTableView.dataSource = self
+        self.wallets = Array(dataManager.getWalletsOrderedByIndex().reversed())
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedWallet = wallets[indexPath.item]
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataManager.walletCount
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let wallet = wallets[indexPath.item]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "walletSelectionReusableCell", for: indexPath) as! walletSelectionReusableCell
+        cell.name.text = wallet.name
+        cell.coinType.text = wallet.coinType
+        cell.coinValue.text = String(wallet.aggregateCoinValue())
+        return cell
+    }
+}
+
+class walletSelectionReusableCell: UITableViewCell {
+    
+    @IBOutlet weak var name: UILabel!
+    
+    @IBOutlet weak var coinType: UILabel!
+    
+    @IBOutlet weak var coinValue: UILabel!
 }
